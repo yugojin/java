8a3c0da9d86c42b0cbaee6840a4750baa2f6168b
yugp jin <yugpjin@yugonoMacBook-Air.local>
2021-01-15 11:06:38 +0900

    ４章

---
+++ Chapter03.java
@@ -0,0 +1,10 @@
+public class Chapter03 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "yugo";
+		System.out.println(str);
+		str = "こんにちは";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
