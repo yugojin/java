84060822feb108eab5468120ad696108397551cf
yugp jin <yugpjin@yugonoMacBook-Air.local>
2021-01-15 16:42:56 +0900

    7章

--- Chapter01.java
+++
@@ -1,5 +0,0 @@
-public class Chapter01 {
-	public static void main(String[] args) {
-        System.out.println("こんにちは!");
-    }
-}
--- Practice01.java
+++
@@ -1,6 +0,0 @@
-public class Chapter02 {
-  public static void main(String[] args) {
-    System.out.println("こんにちは!");
-    System.out.println("Hello World");
-	}
-}
